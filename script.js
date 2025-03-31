43d9634d8b8b436655d1029f7a3483bec21e4248
Thiago Luza <thiagoluza07@gmail.com>
2025-03-31 09:19:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var nome = prompt('Insira seu nome');
+
+var sobrenome = prompt('Insira seu sobrenome ')
+
+window.alert('Olá! ' + nome + ' ' + sobrenome , ', seja bem vindo!');
+
+var numero1 = parseInt(prompt('Insira um número '));
+
+var numero2 = parseInt(prompt('Insira outro número '));
+
+var soma = numero1 + numero2;
+
+window.alert(`A soma dos números é ${soma}`);
+
+console.log('A soma dos números é', numero1 + numero2)
+
